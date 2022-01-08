c51d032cf725c95fa9ef2c96e5cfb301d2ae0c7b
GocasPT <gocasousa2013@gmail.com>
2022-01-08 20:26:29 +0000

    Add TrabalhoV4

---
+++ logica.js
@@ -0,0 +1,17 @@
+var areaSlider = document.getElementById("myAreaInput");
+var idadeSlider = document.getElementById("myIdadeInput");
+
+var areaOutput = document.getElementById("AreaOutput");
+var idadeOutput = document.getElementById("IdadeOutput");
+
+areaOutput.innerHTML = areaSlider.value; // Mostra a o valor default da Área (50)
+idadeOutput.innerHTML = idadeSlider.value; // Mostra a o valor default da Idade (50)
+
+// Atualiza o valor quando a barra mexe
+areaSlider.oninput = function() {
+    areaOutput.innerHTML = this.value;
+} 
+
+idadeSlider.oninput = function() {
+    idadeOutput.innerHTML = this.value;
+} 
