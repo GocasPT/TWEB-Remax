4cd67a13f18f818ed35b160b48f388fab5b143b9
GocasPT <gocasousa2013@gmail.com>
2022-01-10 05:55:10 +0000

    Adição "Zona dos cenários imobiliarios"

--- logica.js
+++ logica.js
@@ -1,17 +1,24 @@
-var areaSlider = document.getElementById("myAreaInput");
-var idadeSlider = document.getElementById("myIdadeInput");
+//Área dos botões "Preview" e "Next"
 
-var areaOutput = document.getElementById("AreaOutput");
-var idadeOutput = document.getElementById("IdadeOutput");
 
-areaOutput.innerHTML = areaSlider.value; // Mostra a o valor default da Área (50)
-idadeOutput.innerHTML = idadeSlider.value; // Mostra a o valor default da Idade (50)
+//Área do "imovel-search"
+    var areaSlider = document.getElementById("myAreaInput");
+    var idadeSlider = document.getElementById("myIdadeInput");
 
-// Atualiza o valor quando a barra mexe
-areaSlider.oninput = function() {
-    areaOutput.innerHTML = this.value;
-} 
+    var areaOutput = document.getElementById("AreaOutput");
+    var idadeOutput = document.getElementById("IdadeOutput");
 
-idadeSlider.oninput = function() {
-    idadeOutput.innerHTML = this.value;
-} 
+    areaOutput.innerHTML = areaSlider.value; // Mostra a o valor default da Área (50)
+    idadeOutput.innerHTML = idadeSlider.value; // Mostra a o valor default da Idade (50)
+
+    // Atualiza o valor quando a barra mexe
+    areaSlider.oninput = function() {
+        areaOutput.innerHTML = this.value;
+    } 
+
+    idadeSlider.oninput = function() {
+        idadeOutput.innerHTML = this.value;
+    }
+
+//Área do "cenarios"
+    
